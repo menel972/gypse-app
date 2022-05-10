effab069e91532d0c3be712053f273360ea9e294
Ménélick <menelick.grandisson@hagergroup.com>
2022-05-10 16:37:11 +0200

    ajout de la font

--- lib/styles/my_text_style.dart
+++ lib/styles/my_text_style.dart
@@ -4,24 +4,33 @@ import 'package:flutter/material.dart';
 class MyTextStyle {
   static TextStyle buttonS = const TextStyle();
   static TextStyle buttonOrangeS = const TextStyle(
+    fontFamily: 'NotoSansDisplay',
     fontSize: 14,
-    fontWeight: FontWeight.w600,
+    letterSpacing: 0.5,
+    fontWeight: FontWeight.w500,
     color: Couleur.secondAccent,
   );
   static TextStyle buttonWhiteM = const TextStyle(
+    fontFamily: 'NotoSansDisplay',
     fontSize: 16,
+    letterSpacing: 1,
     fontWeight: FontWeight.w600,
     color: Colors.white,
   );
   static TextStyle buttonOrangeM = const TextStyle(
+    fontFamily: 'NotoSansDisplay',
     fontSize: 16,
+    letterSpacing: 1,
     fontWeight: FontWeight.w600,
     color: Couleur.secondAccent,
   );
 
   static TextStyle titleS = const TextStyle();
   static TextStyle titleM = const TextStyle(
+    fontFamily: 'NotoSansDisplay',
+    fontWeight: FontWeight.w600,
     fontSize: 24,
+    letterSpacing: 1.5,
     color: Colors.white,
   );
 
@@ -29,7 +38,9 @@ class MyTextStyle {
   static TextStyle textM = const TextStyle();
 
   static TextStyle labelS = const TextStyle(
+    fontFamily: 'NotoSansDisplay',
     fontSize: 14,
+    letterSpacing: 0.5,
   );
   static TextStyle labelM = const TextStyle();
 }
