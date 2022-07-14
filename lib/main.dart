a38a9d973146bdefa2a7881428f56b71bd9b7643
Ménélick <menelick.grandisson@hagergroup.com>
2022-07-14 11:20:38 +0200

    refacto

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ Future<void> main() async {
 
 class Gypse extends StatelessWidget {
   const Gypse({Key? key}) : super(key: key);
-  final String version = '0.5.0';
+  final String version = 'dev_refacto';
 
   // <> Build
   @override
